2d5e23ae76086c0594360c7cd80aeb473f6fed60
rehoy <77278968+rehoy@users.noreply.github.com>
2022-04-06 09:45:43 +0200

    Utils package lagt til metKlassene bruker den

--- app/src/main/java/com/example/in2000team5/data_layer/BicycleRouteRepository.kt
+++ app/src/main/java/com/example/in2000team5/data_layer/BicycleRouteRepository.kt
@@ -7,6 +7,7 @@ import android.location.Geocoder
 import android.location.Location
 import android.util.Log
 import com.example.in2000team5.domain_layer.BicycleViewModel
+import com.example.in2000team5.utils.metUtils
 import com.google.android.gms.maps.model.LatLng
 import org.locationtech.proj4j.CRSFactory
 import org.locationtech.proj4j.CoordinateTransformFactory
@@ -124,26 +125,13 @@ class BicycleRouteRepository {
 
     fun getRealtimeAQI(aqiDataobj : AirQualData?) : Double?{
 
-        val data = aqiDataobj?.data?.time?.find { it.from.equals(getCurrentTimeAsString())}
+        val data = aqiDataobj?.data?.time?.find { it.from.equals(metUtils.getCurrentTimeAsString())}
 
         return data?.variables?.AQI?.value?.toDouble()
 
     }
 
-    fun getCurrentTimeAsString() : String{
 
-        //paddStart sørger for at verdier får 0 foran seg selvom verdien er under 10
-        //Calender.XXXX gir en index hvor verdien finnes i listen
-
-        val year = Calendar.getInstance()[Calendar.YEAR]
-        //Verdien fra måender starter på 0 (Januar er 0)
-        val month = Calendar.getInstance()[Calendar.MONTH].plus(1).toString().padStart(2, '0')
-        val date = Calendar.getInstance()[Calendar.DAY_OF_MONTH].toString().padStart(2, '0')
-        val hour = Calendar.getInstance()[Calendar.HOUR_OF_DAY].toString().padStart(2, '0')
-
-        //Standard reftime for luftkvalitets-API
-        return  "${year}-${month}-${date}T${hour}:00:00Z"
-    }
 
     suspend fun fetchAirQualAtRoute(routeList:List<LatLng>): Double? {
         var avg = 0.0
