73871c0258a6c9ebf51cc84efed67bd4bb9a3286
Ahmed Waseem Saeed <ahmedws@uio.no>
2022-04-25 00:59:48 +0200

    Laget et skjema som kan fylles ut, men ikke mulig å uthente informasjon ennå.

--- app/src/main/java/com/example/in2000team5/ui_layer/AddNewRouteComposeElements.kt
+++ app/src/main/java/com/example/in2000team5/ui_layer/AddNewRouteComposeElements.kt
@@ -1,11 +1,11 @@
 package com.example.in2000team5.ui_layer
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import android.widget.EditText
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -15,8 +15,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotMutableState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.in2000team5.data_layer.BicycleRoute
 import com.example.in2000team5.data_layer.BigBikeRoute
 import com.example.in2000team5.domain_layer.BicycleViewModel
@@ -24,7 +30,7 @@ import com.example.in2000team5.ui_layer.bicycleRouteList
 
 @Composable
 fun VisNyRuteKnapp(bicycleViewModel: BicycleViewModel) {
-    var showForm = remember { mutableStateOf(false) }
+    val showForm = remember { mutableStateOf(false) }
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -50,27 +56,105 @@ fun VisNyRuteKnapp(bicycleViewModel: BicycleViewModel) {
     ) {
 
     }
-    if (showForm.value) VisNyRuteSkjema()
+    if (showForm.value) VisNyRuteSkjema(showForm)
     // TODO: remove form showForm is true
 }
 
+
 @Composable
-fun VisNyRuteSkjema() {
+fun VisNyRuteSkjema(showForm: MutableState<Boolean>) {
+    if (showForm.value) {
+        OpprettDialog(showForm) {
+            BrukerInputView()
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BrukerInputView() {
+    Column(
+        Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Divider()
+        StartOgSluttInput()
+    }
+}
+
+@Composable
+fun OpprettDialog(showForm: MutableState<Boolean>, content: @Composable (() -> Unit)? = null) {
     // TODO: Create the form and make user input values
     Log.d("test", "test klikk")
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        backgroundColor = MaterialTheme.colors.surface,
+    var start = ""
+    AlertDialog(
+        onDismissRequest = { showForm.value = false }, // sjekk ut om dette er riktig
+        title = {
+            Column(
+                Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(text = "Legg til ny rute")
+            }
+        },
+        text = content,
+        confirmButton = {
+            TextButton(onClick = {})
+            { Text(text = "Legg til rute") }
+        },
+        dismissButton = {
+            TextButton(onClick = {})
+            { Text(text = "Avbryt") }
+        },
+        modifier = Modifier.padding(vertical = 8.dp)
+    )
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun StartOgSluttInput(
+    modifier: Modifier = Modifier,
+    start: MutableState<String> = remember {
+        mutableStateOf("")
+    }, slutt: MutableState<String> = remember {
+        mutableStateOf("")
+    }
+) {
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .height(200.dp)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Text(text = "This is a card view",
-                style = MaterialTheme.typography.h4)
-        }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = start.value,
+            onValueChange = { start.value = it },
+            label = { Text("Start") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                keyboardController?.hide()
+                focusRequester.requestFocus()
+            }),
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = slutt.value,
+            onValueChange = { slutt.value = it },
+            label = { Text("Slutt") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                keyboardController?.hide()
+                focusRequester.requestFocus()
+            })
+        )
     }
 }
