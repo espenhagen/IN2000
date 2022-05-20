fda02714051d5116fea2cbc86beda9731782595d
Linn Yuh Gregussen <linn.gregussen@outlook.com>
2022-05-20 15:50:10 +0200

    fiks for null internett

--- app/src/main/java/com/example/in2000team5/ui_layer/MainActivity.kt
+++ app/src/main/java/com/example/in2000team5/ui_layer/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.example.in2000team5.ui_layer
 
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
-import com.example.in2000team5.ui_layer.compose_elements.*
+import com.example.in2000team5.ui_layer.compose_elements.BottomNavigation
 import com.example.in2000team5.ui_layer.theme.IN2000Team5Theme
 import com.example.in2000team5.ui_layer.viewmodels.BicycleInformationViewModel
 import com.example.in2000team5.ui_layer.viewmodels.WeatherDataViewModel
+
 
 class MainActivity : ComponentActivity() {
     private lateinit var bicycleInformationViewModel: BicycleInformationViewModel
     private val weatherDataViewModel: WeatherDataViewModel by viewModels()
 
-    //Kan brukes til å vise om man har internett eller ikke.
-    /*private fun isNetworkConnected(): Boolean {
-        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return cm.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnected
-    }*/
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val internetConnection =  cm.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnected
+
 
         bicycleInformationViewModel = ViewModelProvider(this)[BicycleInformationViewModel::class.java]
 
@@ -33,7 +36,7 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             IN2000Team5Theme {
-                BottomNavigation(weatherDataViewModel, bicycleInformationViewModel)
+                BottomNavigation(internetConnection, weatherDataViewModel, bicycleInformationViewModel)
             }
         }
     }
