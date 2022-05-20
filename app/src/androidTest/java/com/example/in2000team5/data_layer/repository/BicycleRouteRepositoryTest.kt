8922c7b86b839a6ff459a82d535176d476b34e5d
Espen Wigen Hagen <espenwhagen@gmail.com>
2022-05-20 10:35:45 +0200

    Integrasjonstest for fetchRoutes() og sykkel API

--- app/src/androidTest/java/com/example/in2000team5/data_layer/repository/BicycleRouteRepositoryTest.kt
+++ app/src/androidTest/java/com/example/in2000team5/data_layer/repository/BicycleRouteRepositoryTest.kt
@@ -2,6 +2,9 @@ package com.example.in2000team5.data_layer.repository
 
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.in2000team5.data_layer.datasource.remote.BicycleRouteRemoteDataSource
+import com.example.in2000team5.data_layer.datasource.remote.Features
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
 
 import org.junit.Before
@@ -37,6 +40,25 @@ class BicycleRouteRepositoryTest {
         }
         if (y != null) {
             assertEquals(10.785738, y, 0.000001)
+        }
+    }
+
+    @Test
+    fun fetchRoutesTest() {
+        //Integration test of the fetchRoutes()- method and 'Oslo kommune sykkelruter API'.
+        runBlocking {
+
+            //Given
+            val bicycleRouteRemoteDataSource = BicycleRouteRemoteDataSource()
+
+            //When
+            val ruter: List<Features> = bicycleRouteRemoteDataSource.fetchRoutes()!!
+            val rute: Features = ruter.get(0)
+            val ruteId: Number? = rute.properties?.rute?.toInt()
+
+            //Then
+            assertEquals("Rutenummer skal være 4",4,ruteId)
+            assertNotEquals("Sjekker at rutenr ikke er 5",5,ruteId)
         }
     }
 }
